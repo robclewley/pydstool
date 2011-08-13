8bcadd4a7c7529b286ada2e9347338c4581d9de3
Rob Clewley <rob.clewley@gmail.com>
2011-08-13 12:17:02 -0400

    Demonstrated new, improved getEvents and getEventTimes behavior

--- tests/vode_event_test1.py
+++ tests/vode_event_test1.py
@@ -40,9 +40,18 @@ pts = traj.sample()
 plot(pts['t'],pts['w'])
 testODE.diagnostics.showWarnings()
 
-mon_evs_found = testODE.getEvents()['monitor']
-term_evs_found = testODE.getEvents()['threshold']
+mon_evs_found = testODE.getEvents('monitor')
+term_evs_found = testODE.getEvents('threshold')
+
+assert all(traj.getEvents('monitor') == mon_evs_found)
+assert all(traj.getEventTimes('threshold') == testODE.getEventTimes('threshold'))
 term_evs_found.info()
 
-assert len(mon_evs_found) == 2
+# Alternative way to extract events: they are labelled in the
+# pointset! These return dictionaries indexing into the pointset.
+mon_evs_dict = pts.labels.by_label['Event monitor']
+mon_ev_points = pts[sort(mon_evs_dict.keys())]
+
+assert len(mon_evs_found) == len(mon_ev_points) == 2
+assert numpy.all(mon_evs_found == mon_ev_points)
 
