1e7d1c3ee7c876c3ad048a6fcdcea432c528c277
Rob Clewley <rob.clewley@gmail.com>
2011-09-05 01:59:45 -0400

    Added testing for embedded for/sum/if macros in specs

--- tests/basic_gen_tests.py
+++ tests/basic_gen_tests.py
@@ -120,22 +120,44 @@ sintraj2(0.8, checklevel=2)
 fnspecs = {'testif': (['x'], 'if(x<0.0,0.0,x)'),
           'testmin': (['x', 'y'], 'min(x,y)'),
           'testmax': (['x', 'y'], 'max(x,y)'),
-          'testmin2': (['x', 'y'], '1/(2+min(1+(x*3),y))+y')
+          'testmin2': (['x', 'y'], '1/(2+min(1+(x*3),y))+y'),
+          'indexfunc': (['i'], 'pi*i')
           }
 
 DSargs = args(name='test',
               pars={'p0': 0, 'p1': 1, 'p2': 2},
-              varspecs={'z[i]': 'for(i, 0, 1, 2*z[i+1] + p[i])',
+              varspecs={'z[j]': 'for(j, 0, 1, 2*z[j+1] + p[j])',
                    'z2': '-z0 + p2'},
               fnspecs=fnspecs
               )
 tmm = Generator.Vode_ODEsystem(DSargs)
 
-# test user interface to aux functions
+# test user interface to aux functions and different combinations of embedded
+# macros
 
 assert tmm.auxfns.testif(1.0) == 1.0
 assert tmm.auxfns.testmin(1.0, 2.0) == 1.0
 assert tmm.auxfns.testmax(1.0, 2.0) == 2.0
 assert tmm.auxfns.testmin2(1.0, 2.0) == 2.25
 assert tmm.Rhs(0, {'z0': 0.5, 'z1': 0.2, 'z2': 2.1})[1] == 5.2
+
+DSargs2 = args(name='test2',
+              pars={'p0': 0, 'p1': 1, 'p2': 2},
+              varspecs={'y[i]': 'for(i, 0, 1, y[i]+if(y[i+1]<2, 2+p[i]+getbound(y2,0), indexfunc([i])+y[i+1]) - 3)',
+                        'y2': '0'
+                        },
+              xdomain={'y2': [-10,10]},
+              fnspecs=fnspecs
+              )
+tm2 = Generator.Vode_ODEsystem(DSargs2)
+assert allclose(tm2.Rhs(0, {'y0':0, 'y1': 0.3, 'y2': 5}), array([-11. ,  2.3+pi,  0. ]))
+
+DSargs3 = args(name='test3',
+              pars={'p0': 0, 'p1': 1, 'p2': 2},
+              varspecs={'x': 'sum(i, 0, 4, sum(j, 0, 1, indexfunc([j] + p[j])))'}, #'sum(j, 0, 1, indexfunc([j] + p[j]))'}, #'sum(i, 0, 4, sum(j, 0, 1, index_func([j] + p[j])))'},
+              fnspecs=fnspecs
+              )
+tm3 = Generator.Vode_ODEsystem(DSargs3)
+assert allclose(tm3.Rhs(0, {'x': 0}), 10*pi)
+
 print "  ...passed"
