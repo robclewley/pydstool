b0fd477a425356e9d6fb5f1b53129fc6cb7691e8
Vladimir Zakharov <zakharov.vv@gmail.com>
2014-02-26 15:24:47 +0400

    common: use 'set' internal type to ensure uniqueness

---
+++ PyDSTool/tests/test_common.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function
+
+import numpy
+
+from PyDSTool.common import (
+    isUniqueSeq,
+    makeSeqUnique,
+)
+
+
+def test_isUniqueSeq_returns_true_for_empty_list():
+    assert isUniqueSeq([])
+
+
+def test_isUniqueSeq_returns_true_for_list_with_unique_items():
+    assert isUniqueSeq(['1', '2', '3'])
+
+
+def test_isUniqueSeq_returns_false_for_list_with_duplicated_items():
+    assert not isUniqueSeq(['1', '2', '1'])
+
+
+def test_makeSeqUnique_returns_list_with_unique_items():
+    assert isUniqueSeq(makeSeqUnique(['1', '2', '1']))
+
+
+def test_makeSeqUnique_returns_array_when_asarray_set():
+    assert isinstance(makeSeqUnique([1, 2, 1], asarray=True), numpy.ndarray)
+
+
+def test_makeSeqUnique_returns_array_with_unique_items():
+    assert isUniqueSeq(makeSeqUnique([1, 2, 1], asarray=True))
+
+
+def test_makeSeqUnique_preserves_order():
+    assert makeSeqUnique('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D']
