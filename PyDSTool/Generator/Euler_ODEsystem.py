e1f3cefd754fd841840a51259341ecc48d2e264f
Vladimir Zakharov <zakharov.vv@gmail.com>
2014-04-10 10:44:46 +0400

    Py3: make 'Generator/Euler_ODEsystem.py' compatible with Python 3.3

--- PyDSTool/Generator/Euler_ODEsystem.py
+++ PyDSTool/Generator/Euler_ODEsystem.py
@@ -1,6 +1,6 @@
 """Euler integrator for ODE systems, with no step refinement for events.
 """
-from __future__ import division, absolute_import
+from __future__ import division, absolute_import, print_function
 
 from .allimports import *
 from PyDSTool.Generator import ODEsystem as ODEsystem
@@ -87,7 +87,7 @@ class Euler_ODEsystem(ODEsystem):
                          'init_step': 0.01,
                          'max_pts': 100000
                          }
-        for k, v in algparams_def.iteritems():
+        for k, v in algparams_def.items():
             if k not in self.algparams:
                 self.algparams[k] = v
 
@@ -125,7 +125,7 @@ class Euler_ODEsystem(ODEsystem):
         indepdom0, indepdom1 = self.indepvariable.depdomain.get()
         if continue_integ:
             if indepdom0 > self._solver.t:
-                print "Previous end time is %f"%self._solver.t
+                print("Previous end time is %f"%self._solver.t)
                 raise ValueError("Start time not correctly updated for "
                                  "continuing orbit")
             x0 = self._solver.y
@@ -169,7 +169,7 @@ class Euler_ODEsystem(ODEsystem):
         auxvarsfn = getattr(self,self.funcspec.auxspec[1])
         # Make t mesh if it wasn't given as 'specialtimes'
         if not all(isfinite(self.indepvariable.depdomain.get())):
-            print "Time domain was: ", self.indepvariable.depdomain.get()
+            print("Time domain was: %r" % self.indepvariable.depdomain.get())
             raise ValueError("Ensure time domain is finite")
         if dt == indepdom1 - indepdom0:
             # single-step integration required
@@ -197,13 +197,13 @@ class Euler_ODEsystem(ODEsystem):
             try:
                 allaDataDict[aname] = [avals[aix]]
             except IndexError:
-                print "\nEuler generator: There was a problem evaluating " \
-                      + "an auxiliary variable"
-                print "Debug info: avals (length", len(avals), ") was ", avals
-                print "Index out of range was ", aix
-                print self.funcspec.auxspec[1]
-                print hasattr(self, self.funcspec.auxspec[1])
-                print "Args were:", [t0, x0, extralist]
+                print("\nEuler generator: There was a problem evaluating " \
+                      + "an auxiliary variable")
+                print("Debug info: avals (length %d) was %r" % (len(avals), avals))
+                print("Index out of range was %d" % aix)
+                print(self.funcspec.auxspec[1])
+                print(hasattr(self, self.funcspec.auxspec[1]))
+                print("Args were: %r" % [t0, x0, extralist])
                 raise
         # Initialize signs of event detection objects at IC
         self.setEventICs(self.initialconditions, self.globalt0)
@@ -276,11 +276,11 @@ class Euler_ODEsystem(ODEsystem):
             try:
                 errcode = solver.integrate(dt)
             except:
-                print "Error calling right hand side function:"
+                print("Error calling right hand side function:")
                 self.showSpec()
-                print "Numerical traceback information (current state, " \
-                      + "parameters, etc.)"
-                print "in generator dictionary 'traceback'"
+                print("Numerical traceback information (current state, " \
+                      + "parameters, etc.)")
+                print("in generator dictionary 'traceback'")
                 self.traceback = {'vars': dict(zip(xnames,solver.y)),
                                   'pars': dict(zip(pnames,plist)),
                                   'inputs': dict(zip(inames,ilist)),
@@ -307,9 +307,8 @@ class Euler_ODEsystem(ODEsystem):
                                                             eventslist)
 ##                print new_t, evsflagged
 ##                evsflagged = [ev for ev in evsflagged if solver.t-indepdom0 > ev[1].eventinterval]
-                termevsflagged = filter(lambda e: e in evsflagged, termevents)
-                nontermevsflagged = filter(lambda e: e not in termevsflagged,
-                                           evsflagged)
+                termevsflagged = [e for e in termevents if e in evsflagged]
+                nontermevsflagged = [e for e in evsflagged if e not in termevsflagged]
                 # register any non-terminating events in the warnings
                 # list, unless they are 'precise' in which case flag
                 # them to be resolved after integration completes
@@ -384,7 +383,7 @@ class Euler_ODEsystem(ODEsystem):
             # much less accurately determined)
             if not breakwhile:
                 # only here if a terminal event hasn't just flagged
-                for xi in xrange(self.dimension):
+                for xi in range(self.dimension):
                     if not self.contains(depdomains[xi],
                                      solver.y[xi],
                                      self.checklevel):
@@ -414,17 +413,18 @@ class Euler_ODEsystem(ODEsystem):
                                                          self.checklevel) \
                                                   for f in inputVarList]
                 except ValueError:
-                    print 'External input call caused value out of range error:',\
-                          't = ', solver.t
+                    print('External input call caused value out of range error:',\
+                          't = %f' % solver.t)
                     for f in inputVarList:
                         if f.diagnostics.hasWarnings():
-                            print 'External input variable %s out of range:'%f.name
-                            print '   t = ', repr(f.diagnostics.warnings[-1][0]), ', ', \
-                                  f.name, ' = ', repr(f.diagnostics.warnings[-1][1])
+                            print('External input variable %s out of range:'%f.name)
+                            print('   t = %r, %s = %r' % (
+                                repr(f.diagnostics.warnings[-1][0]),
+                                f.name,
+                                repr(f.diagnostics.warnings[-1][1])))
                     raise
                 except AssertionError:
-                    print 'External input call caused t out of range error: t = ', \
-                          solver.t
+                    print('External input call caused t out of range error: t = %f' % solver.t)
                     raise
                 solver.set_f_params(extralist)
                 breakwhile = not solver.successful()
@@ -437,8 +437,8 @@ class Euler_ODEsystem(ODEsystem):
             try:
                 if self.diagnostics.warnings[-1][0] not in [W_TERMEVENT,
                                                             W_TERMSTATEBD]:
-                    print "t =", solver.t
-                    print "state =", dict(zip(xnames,solver.y))
+                    print("t =%f" % solver.t)
+                    print("state =%r" % dict(zip(xnames,solver.y)))
                     raise RuntimeError("Event finding code for terminal event "
                                        "failed in Generator " + self.name + \
                                        ": try decreasing eventdelay or "
@@ -446,8 +446,8 @@ class Euler_ODEsystem(ODEsystem):
                                        "atol and rtol parameters")
             except IndexError:
                 info(self.diagnostics.outputStats, "Output statistics")
-                print "t =", solver.t
-                print "x =", solver.y
+                print("t =%f" % solver.t)
+                print("x =%f" % solver.y)
                 raise RuntimeError("Event finding failed in Generator " + \
                                    self.name + ": try decreasing eventdelay "
                                    "or eventinterval below eventtol")
@@ -485,18 +485,18 @@ class Euler_ODEsystem(ODEsystem):
                     interp = interp1d(alltData, xvals)
                 variables[x] = Variable(interp, 't', x, x)
             else:
-                print "Error in Generator:", self.name
-                print "t = ", alltData
-                print "x = ", allxDataDict
+                print("Error in Generator:%s" % self.name)
+                print("t = %r" % alltData)
+                print("x = %r" % allxDataDict)
                 raise PyDSTool_ValueError("Fewer than 2 data points computed")
         for a in anames:
             if len(alltData) > 1:
                 variables[a] = Variable(interp1d(alltData, allaDataDict[a]),
                                         't', a, a)
             else:
-                print "Error in Generator:", self.name
-                print "t = ", alltData
-                print "x = ", allxDataDict
+                print("Error in Generator:%s" % self.name)
+                print("t = %r" % alltData)
+                print("x = %r" % allxDataDict)
                 raise PyDSTool_ValueError("Fewer than 2 data points computed")
         self.diagnostics.outputStats = {'last_step': dt,
                             'num_fcns': num_points,
@@ -505,7 +505,7 @@ class Euler_ODEsystem(ODEsystem):
                             }
         if solver.successful():
             #self.validateSpec()
-            for evname, evtlist in Evtimes.iteritems():
+            for evname, evtlist in Evtimes.items():
                 try:
                     self.eventstruct.Evtimes[evname].extend([et+self.globalt0 \
                                             for et in evtlist])
@@ -526,7 +526,7 @@ class Euler_ODEsystem(ODEsystem):
                                                'indepvararray': Evtimes[evname],
                                                'indepvartype': float})
             self.defined = True
-            return Trajectory(trajname, variables.values(),
+            return Trajectory(trajname, list(variables.values()),
                               abseps=self._abseps, globalt0=self.globalt0,
                               checklevel=self.checklevel,
                               FScompatibleNames=self._FScompatibleNames,
@@ -540,7 +540,7 @@ class Euler_ODEsystem(ODEsystem):
                                     self.diagnostics._errorcodes[errcode])))
             except TypeError:
                 # e.g. when errcode has been used to return info list
-                print "Error information: ", errcode
+                print("Error information: %d" % errcode)
                 self.diagnostics.errors.append((E_COMPUTFAIL, (solver.t,
                                     self.diagnostics._errorcodes[0])))
             self.defined = False
@@ -561,7 +561,7 @@ class Euler_ODEsystem(ODEsystem):
             p = sortedDictValues(self._FScompatibleNames(pdict))
         i = _pollInputs(sortedDictValues(self.inputs),
                         t, self.checklevel)
-        return apply(getattr(self, self.funcspec.spec[1]), [t, x, p+i])
+        return getattr(self, self.funcspec.spec[1])(*[t, x, p+i])
 
 
     def Jacobian(self, t, xdict, pdict=None, asarray=True):
@@ -579,8 +579,7 @@ class Euler_ODEsystem(ODEsystem):
                 p = sortedDictValues(self._FScompatibleNames(pdict))
             i = _pollInputs(sortedDictValues(self.inputs),
                         t, self.checklevel)
-            return apply(getattr(self, self.funcspec.auxfns["Jacobian"][1]), \
-                         [t, x, p+i])
+            return getattr(self, self.funcspec.auxfns["Jacobian"][1])(*[t, x, p+i])
         else:
             raise PyDSTool_ExistError("Jacobian not defined")
 
@@ -600,8 +599,7 @@ class Euler_ODEsystem(ODEsystem):
                 p = sortedDictValues(self._FScompatibleNames(pdict))
             i = _pollInputs(sortedDictValues(self.inputs),
                         t, self.checklevel)
-            return apply(getattr(self, self.funcspec.auxfns["Jacobian_pars"][1]), \
-                        [t, x, p+i])
+            return getattr(self, self.funcspec.auxfns["Jacobian_pars"][1])(*[t, x, p+i])
         else:
             raise PyDSTool_ExistError("Jacobian w.r.t. parameters not defined")
 
@@ -620,7 +618,7 @@ class Euler_ODEsystem(ODEsystem):
             p = sortedDictValues(self._FScompatibleNames(pdict))
         i = _pollInputs(sortedDictValues(self.inputs),
                         t, self.checklevel)
-        return apply(getattr(self, self.funcspec.auxspec[1]), [t, x, p+i])
+        return getattr(self, self.funcspec.auxspec[1])(*[t, x, p+i])
 
 
     def __del__(self):
