41e8dd2443216e3be4870e097d6c3cf99b3e9375
Vladimir Zakharov <zakharov.vv@gmail.com>
2014-04-18 11:45:04 +0400

    Py3: make 'Toolbox/ModelHelper.py' compatible with Python 3.3

--- PyDSTool/Toolbox/ModelHelper.py
+++ PyDSTool/Toolbox/ModelHelper.py
@@ -7,7 +7,7 @@
 
   2008: This is superceded by GDescriptor class. See PyDSTool/tests/
 """
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 from PyDSTool import *
 from copy import copy
@@ -112,60 +112,60 @@ def makeSpiffyODEModel(**kw):
 
     if targetGen not in allODEgens:
         # Print a list of the valid targets
-        print 'Valid target ODE solvers: ' + ", ".join(allODEgens)
+        print('Valid target ODE solvers: ' + ", ".join(allODEgens))
         raise ValueError('Invalid target ODE solver')
 
     # Empty model spec to be populated with the information in modelDict
     theModelSpec = SpiffyODEModel(modelName, compatibleGens=allODEgens)
 
     if not silent:
-        print "Building model: '%s'"%modelName
+        print("Building model: '%s'"%modelName)
 
     # Build the ODE RHS, save list of ODEs
     varnames = []
     if 'odes' in modelDict.keys():
-        for odeName, expr in modelDict['odes'].iteritems():
+        for odeName, expr in modelDict['odes'].items():
             theModelSpec.add(Var(expr, odeName, specType='RHSfuncSpec'))
             varnames.append(odeName)
             if not silent:
-                print 'Added ODE: ', odeName
+                print('Added ODE: ', odeName)
 
     # Build the aux variables, save list of them
     auxvarnames = []
     if 'auxs' in modelDict.keys():
-        for auxName, expr in modelDict['auxs'].iteritems():
+        for auxName, expr in modelDict['auxs'].items():
             theModelSpec.add(Var(expr, auxName, specType='ExpFuncSpec'))
             auxvarnames.append(auxName)
             if not silent:
-                print 'Added aux variable: ', auxName
+                print('Added aux variable: ', auxName)
 
     # Build the list of RHS specific parameters, save list of them
     parnames = []
     if 'params' in modelDict.keys():
-        for parName, val in modelDict['params'].iteritems():
+        for parName, val in modelDict['params'].items():
             theModelSpec.add(Par(str(val), parName))
             parnames.append(parName)
             if not silent:
-                print 'Added parameter: ', parName, "=", val
+                print('Added parameter: ', parName, "=", val)
 
     # Build the inputs, save list of them
     inputnames = []
     inputdict = {}
 
     if 'inputs' in modelDict.keys():
-        for inputName, val in modelDict['inputs'].iteritems():
+        for inputName, val in modelDict['inputs'].items():
             theModelSpec.add(Input(str(inputName)))
             inputnames.append(inputName)
             #if not silent:
             #    print val.variables.keys()
             inputdict[inputName] = val.variables[inputName]
             if not silent:
-                print 'Added input: ', inputName, "=", val
+                print('Added input: ', inputName, "=", val)
 
     # Build the dict of extra functions, save list of them
     auxfndict = {}
     if 'functions' in modelDict.keys():
-        for funSig, expr in modelDict['functions'].iteritems():
+        for funSig, expr in modelDict['functions'].items():
             # check syntax; this could be taken care of by Lex-built
             # syntax checking function
             assert ')' == funSig[-1]
@@ -183,7 +183,7 @@ def makeSpiffyODEModel(**kw):
             if name == 'Jacobian_pars':
                 makeJacP = False
             if not silent:
-                print 'Added function: ', name, " of arguments: ", args
+                print('Added function: ', name, " of arguments: ", args)
 
 
     # Make symbolic jacobian if requested
@@ -195,7 +195,7 @@ def makeSpiffyODEModel(**kw):
             Jac = Fun(Diff(F, varnames), ['t'] + varnames, 'Jacobian')
             theModelSpec.add(Jac)
             if not silent:
-                print "Added jacobian"
+                print("Added jacobian")
 
     if makeJacP:
         if 'Jacobian_pars' not in modelDict.keys():
@@ -206,7 +206,7 @@ def makeSpiffyODEModel(**kw):
             JacP = Fun(Diff(G, varnames + parnames), ['t'] + varnames + parnames, 'Jacobian_pars')
             theModelSpec.add(JacP)
             if not silent:
-                print "Added jacobian w.r.t parameters"
+                print("Added jacobian w.r.t parameters")
 
 
     # Construct the model
