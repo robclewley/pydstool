8409e924f0418d31e5c3c228d78eb926c7047067
Vladimir Zakharov <zakharov.vv@gmail.com>
2018-08-14 17:04:07 +0300

    PyCont/TestFunc.py: add division import

--- PyDSTool/PyCont/TestFunc.py
+++ PyDSTool/PyCont/TestFunc.py
@@ -22,7 +22,7 @@
 #
 # ----------------------------------------------------------------------------
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, division, print_function
 
 from .misc import *
 from PyDSTool.common import args, copy
